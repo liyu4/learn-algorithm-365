6e5521c4c1f395f4da558372df028af2212bf701
liyu4 <yangxiaoxiu@live.cn>
2016-11-23 09:46:53 +0800

    add merger sort that average cost O(nlogn)

---
+++ sort/merge_sort.go
@@ -0,0 +1,52 @@
+package sort
+
+import ()
+
+func merge(left, right []int) {
+	arr := make([]int, len(left)+len(right))
+
+	// index p for left, q for right
+
+	p, q := 0, 0
+
+	for i := 0; i < len(arr); i++ {
+
+		// fix for index out of range without using sentinel
+		{
+			if p >= len(left) {
+				arr[i] = right[q]
+				j++
+				continue
+			} else if q >= len(right) {
+				arr[i] = left[p]
+				i++
+				continue
+			}
+		}
+
+		// default loop conditon define in algorithm
+		if left[p] >= right[q] {
+			arr[i] = right[q]
+			q = q + 1
+		} else {
+			arr[i] = left[p]
+			p = p + 1
+		}
+	}
+}
+
+func sort(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	mid := len(array) / 2
+
+	left := array[:mid]
+	right := array[mid:]
+
+	left = sort(left)
+	right = sort(right)
+
+	return merge(left, right)
+}
