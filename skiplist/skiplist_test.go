e49a39e5123c9e41de3e8ff36383e047516dcb67
liyu4 <yangxiaoxiu@live.cn>
2016-11-24 23:17:32 +0800

    add skiplist_test file to jtest the insert method

---
+++ skiplist/skiplist_test.go
@@ -0,0 +1,21 @@
+package skiplist
+
+import (
+	"testing"
+)
+
+func TestSkiplistInsert(t *testing.T) {
+	l := newList()
+	insert(l, 31, 5)
+	insert(l, 1, 3)
+	insert(l, 31, 6)
+	insert(l, 20, 7)
+	insert(l, 20, 2)
+
+	for _, v := range l.header.forward {
+		if v != nil {
+			t.Log(v.key)
+			t.Log(v.value)
+		}
+	}
+}
