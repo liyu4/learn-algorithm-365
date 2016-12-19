25ab007ffa4b82e644e2c2e8e5eebd027b602fef
liyu4 <yangxiaoxiu@live.cn>
2016-12-19 23:51:07 +0800

    add left_rotate function

--- tree/rbt.go
+++ tree/rbt.go
@@ -2,7 +2,7 @@
 package tree
 
 import (
-	"fmt"
+// "fmt"
 )
 
 type Color byte
@@ -23,9 +23,46 @@ type Rbtree struct {
 	sentinel *RBNode // a sentry or guard
 }
 
+//  初始化树
 func (r *Rbtree) Init() {
 	var sentinel RBNode
-	sentinel.color = 2
+	sentinel.color = BLACK
 	r.root = &sentinel     // 根结点为一个黑色的空哨兵
 	r.sentinel = &sentinel // 哨兵节点
+}
+
+func NewRbtree() *Rbtree {
+	var sentinel RBNode
+	sentinel.color = BLACK
+	return &Rbtree{root: &sentinel, sentinel: &sentinel}
+}
+
+func (r *Rbtree) left_rotate(node *RBNode) {
+	var temp *RBNode
+
+	// 此处元素为node节点的右子树
+	temp = node.right
+	// 转换temp的左子树成为node的左子树
+	node.right = temp.left
+
+	if temp.left != r.sentinel {
+		temp.left.parent = node
+	}
+
+	temp.parent = node.parent
+
+	if node.parent == r.sentinel {
+		r.root = temp
+	} else if node == node.parent.left {
+		temp.parent.left = temp
+	} else {
+		node.parent.right = temp
+	}
+
+	temp.left = node
+	node.parent = temp
+}
+
+func (r *Rbtree) right_rotate(node *RBNode) {
+
 }
