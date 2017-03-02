d99457ccda938e8ae66d98f0bfa9b8fa066f625a
liyu4 <yangxiaoxiu@live.cn>
2017-03-02 11:54:07 +0800

    for clean

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
+type MyStruct struct {
+	name string
+}
+
+func (this *MyStruct) GetName() string {
+	return this.name
+}
+
 func main() {
-	fmt.Println("hello come here")
+	s := "this is string"
+	fmt.Println(reflect.TypeOf(s))
+	fmt.Println("-------------------")
+
+	fmt.Println(reflect.ValueOf(s))
+	var x float64 = 3.4
+	fmt.Println(reflect.ValueOf(x))
+	fmt.Println("-------------------")
+
+	a := new(MyStruct)
+	a.name = "yejianfeng"
+	typ := reflect.TypeOf(a)
+
+	fmt.Println(typ.NumMethod())
+	fmt.Println("-------------------")
+
+	b := reflect.ValueOf(a).MethodByName("GetName").Call([]reflect.Value{})
+	fmt.Println(b[0])
+
 }
---
+++ shortestpath/dijkastra.go
@@ -0,0 +1,17 @@
+// dijkstra implement of dijkstra's algorithm
+// which is an algorithm for finding the shortest paths between nodes in a graph.
+
+package dijkstra
+
+import (
+	"fmt"
+)
+
+const (
+	num = 10
+)
+
+var (
+	element  [num][num]int
+	distance [num]int
+)
